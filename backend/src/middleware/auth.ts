a7245b79a5112af89923c495c0226b0518f3d046
Qadirmemon90 <myaqoobmamen669@gmail.com>
2026-09-18 12:50:03 +0500

    fix: handle errors correctly in auth middleware and user retrieval functions

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -13,7 +13,7 @@ export const protectRoute = [
   async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
       const { userId: clerkId } = getAuth(req);
-      if (!clerkId) return res.status(401).json({ message: "Unauthorized" });
+      // if (!clerkId) return res.status(401).json({ message: "Unauthorized" });
 
       const user = await User.findOne({ clerkId });
       if (!user) return res.status(404).json({ message: "User not found" });
@@ -22,8 +22,8 @@ export const protectRoute = [
       
       next();
     } catch (error) {
-      console.error("Error in protectRoute middleware", error)
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500);
+      next(error)
     }
   },
 ];
