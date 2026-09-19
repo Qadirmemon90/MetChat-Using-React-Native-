e19efcc92a18ca27366dd34e2e2394d783f0fdf0
Qadirmemon90 <myaqoobmamen669@gmail.com>
2026-09-19 20:27:27 +0500

    feat: implement chat and message controllers with route protection

--- backend/src/Routes/messageRoutes.ts
+++ backend/src/Routes/messageRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getMessages } from "../controllers/messageConrollers";
 
 const router = Router();
 
-
+router.get("/chat/:chatId", protectRoute,getMessages)
 
 
 export default router;
