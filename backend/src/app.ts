8743267a760fa2c9e502a1d45efaa6565fb3b15e
Qadirmemon90 <myaqoobmamen669@gmail.com>
2026-09-20 20:47:15 +0500

    feat: add Dockerfile for application setup and include .dockerignore

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+
 import { clerkMiddleware } from '@clerk/express'
 
 import authRoutes from "./Routes/authRoutes";
@@ -23,5 +25,14 @@ app.use("/api/message", messageRoutes);
 app.use("/api/user", userRoutes);
 
 app.use(errorHandler);
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../../web/dist")));
+
+  app.get("/{*any}", (_, res)=>{
+    res.sendFile(path.join(__dirname,"../../web/dist/index.html"))
+  })
+}
 
 export default app;
